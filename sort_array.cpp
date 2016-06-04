c95efdbfb617115c5164b5f7257d36de154d9faa
Dustin Van Tate Testa <toast27@gmail.com>
2016-06-04 19:11:45 -0400

    My sorting algorithm/function :)
    I'm still relativly certain that there is an improvement that I could do to optimize this function.
    - templates
    - pointers
    - treating arrays as pointers (because they are)
    - I would consider this a somewhat advanced use of pointers/arrays, at least in the scope of this repo.
    - operators
    - loops
    - if-statements
    - functions

---
+++ sort_array.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+// this function is the core of this program :D
+template<class T>
+void sortArray(T* arr, size_t length){
+	
+	//for each element of the array...
+	for (length; length > 0; length--, arr++) {
+
+		T maxValue = *arr;
+		T* swapVal = arr;
+
+		//find the largest remaining number
+		for (size_t i = 0; i < length; i++)			
+			if (maxValue < *(arr + i)) {
+				maxValue = *(arr + i);
+				swapVal = arr + i;	
+			}
+		
+		// switch elements' locations in the array
+		T temp = *arr;
+		*arr = *swapVal;
+		*swapVal = temp;
+	}
+
+}
+
+
+int main(){
+
+	std::cout <<"How many numbers to sort?\n";
+	
+	size_t length; // NOTE: size_t = long int
+	std::cin >>length;
+
+	// allocate some memory to hold our array
+	long double userArray[length];
+
+	std::cout <<"Enter " <<length <<" numbers:\n";
+
+	for (size_t i = 0; i < length; i++)
+		std::cin >>userArray[i];
+	
+	//sort the array
+	sortArray(userArray, length);	
+
+
+	// print the array
+	std::cout <<"\nin numerical order:\n";
+ 
+ 	std::cout <<userArray[0];	
+	for (size_t i = 1; i < length; i++)
+		std::cout <<", " <<userArray[i];
+
+	// terminating newlines are nice
+	std::cout <<std::endl;
+
+
+}
