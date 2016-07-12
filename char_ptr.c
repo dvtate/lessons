1e8d1005c1f961d36255e538e3cc8b6dbf124515
Dustin Van Tate Testa <toast27@gmail.com>
2016-07-12 13:47:42 -0400

    using a pointer as an array
    refer to char_arr this is one C equivalent. I like to mix C and C++ code together nicely :)
    - pointers 
    - arrays
    - c
    - printf()
    - chars

--- char_ptr.c
+++ char_ptr.c
@@ -1,4 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> // printf()
+
+#include <stdlib.h> // malloc()
+#include <string.h> // strcpy()
 
 // C is a subset of C++...
 // I only made this a C program because modern C++ compilers give 
@@ -10,14 +13,32 @@
 
 int main(){
 
+	// declaring our strings
 	char *firstWord, *secondWord;
 
-        // this also allocates memory :D
-	firstWord = "HAPPY"; // 'H', 'A', 'P', 'P', 'Y', '\0'
+	// although the compiler is smart enough to allocate the
+	// memory needed by these strings for you, it isn't a good
+	// practice to rely on this, and is frowned upon by the standards.
+	// we can use malloc to allocate some memory :)
+	
+	// there are 6 characters in "HAPPY" including the null terminator
+	// each character uses 1 byte of memory so we allocate 6 bytes and
+	// point firstWord to the empty memory.
+	firstWord = (char*) malloc(6); // 'H', 'A', 'P', 'P', 'Y', '\0'
+	secondWord = (char*) malloc(10);
 
-	secondWord = "BIRTHDAY!";
+	// copy the words into the memory you have allocated.
+	strcpy(firstWord, "HAPPY");
+	strcpy(secondWord,"BIRTHDAY!");
 
-        // std::cout is easier IMO...
+        // lets print the words to the console :)
 	printf("%s %s\n", firstWord, secondWord);
+
+
+	// any memory allocated via malloc must be free()'ed to prevent
+	// memory leaks. Here it isn't needed since the memory is freed
+	// when the program ends.
+	free(firstWord);
+	free(secondWord);
 
 }
