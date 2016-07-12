b72d0f2276f5e8e38e1bbf75676de289db0f2edb
Dustin Van Tate Testa <toast27@gmail.com>
2016-07-12 13:28:23 -0400

    determines if number is even or odd
    - operators
    - if-statement
    - types
    
    # update added more comments and removed unneeded curly braces.

--- even-odd_test.cpp
+++ even-odd_test.cpp
@@ -8,11 +8,10 @@ int main(){
 	std::cin >>n;
 	
 	// % = remainder operator (aka modulus (mod))
-	if (n % 2 == 0) {
-		std::cout <<std::endl <<n <<" is even, (or an error)" <<std::endl;
-	} else {
+	if (n % 2 == 0) // if the number is evenly divisible by 2 then it's even.
+		std::cout <<std::endl <<n <<" is even, (or is it an error?)" <<std::endl;
+	else
 		std::cout <<std::endl <<n <<" is an odd number" <<std::endl;
-	}
 }
 
 
