f6027cd7a22c85f8870393674c8c81e2d53b9686
Dustin Van Tate Testa <toast27@gmail.com>
2016-09-18 12:19:09 -0400

    predicts the future (100% accurate)
    - comparison
    - getline (for getting strings which have ' ' spaces in them)
    - if-statement
    -

--- fortune_teller.cpp
+++ fortune_teller.cpp
@@ -1,24 +1,29 @@
 #include<iostream>
 
-using namespace std;
-
 
 int main(){
-	
-	string name;
 
-	char age;
-	
-	cout <<"I'm a  fortune teller.\nUnfortionatly, I have no idea who you are.\n So what's your name? ";;
-	getline(cin, name);
+        std::string name;
 
-	cout <<"I'm afraid there's more than one " <<name <<".\nSo how old are you?\n";
-	cin >>age;
+        short int age;
+
+        std::cout <<"I'm a  fortune teller.\nUnfortionatly, I have no idea who you are.\n So what's your name? ";;
+        getline(std::cin, name);
+
+        std::cout <<"I'm afraid there's more than one " <<name <<".\nSo how old are you? ";
+        std::cin >>age;
 
 
-	if (age <= 0 || age > 200)
-		cout <<"\nThere's no way you're " <<age <<"!";
-	else
-		cout <<"\nOk, I can assure you that in 10 rears, you will be " 
-			 <<(age + 10) <<'.' <<std::endl;
+        if (age <= 0 || age > 200)
+                std::cout <<"\nThere's no way you're " <<age <<"!";
+        else
+                std::cout <<"\nOk, I can assure you that in 10 rears, you will be "
+                          <<age + 10 <<'.' <<std::endl;
 }
+
+
+
+
+
+
+
