f7e1b19d38735e05e22c44a2e7930ae2cf933674
Dustin Van Tate Testa <toast27@gmail.com>
2016-09-30 14:28:26 -0700

    My sorting algorithm/function :)
    I'm relativly certain that there is an improvement that I could do to optimize this function.
    - templates
    - pointers
    - treating arrays as pointers (because they are)
    - I would consider this a somewhat advanced use of pointers/arrays, at least in the scope of this repo.
    - operators
    - loops
    - if-statements
    - functions

--- sort_array.cpp
+++ sort_array.cpp
@@ -2,58 +2,56 @@
 
 // this function is the core of this program :D
 template<class T>
-void sortArray(T* arr, size_t length){
-	
-	//for each element of the array...
-	for (length; length > 0; length--, arr++) {
+  void sortArray(T* arr, size_t length){
+  // for each element of the array...
+  for (; length > 0; length--, arr++) {
 
-		T maxValue = *arr;
-		T* swapVal = arr;
+    T maxValue = *arr;
+    T* swapVal = arr;
 
-		//find the largest remaining number
-		for (size_t i = 0; i < length; i++)			
-			if (maxValue < *(arr + i)) {
-				maxValue = *(arr + i);
-				swapVal = arr + i;	
-			}
-		
-		// switch elements' locations in the array
-		T temp = *arr;
-		*arr = *swapVal;
-		*swapVal = temp;
-	}
-
+    //find the largest remaining number
+    for (size_t i = 0; i < length; i++) 
+      if (maxValue < *(arr + i))
+	maxValue = *(swapVal = arr + i);
+    
+    // switch elements' locations in the array
+    T temp = *arr;
+    *arr = *swapVal;
+    *swapVal = temp;
+    
+  }
+  
 }
 
 
 int main(){
 
-	std::cout <<"How many numbers to sort?\n";
+  std::cout <<"How many numbers to sort?\n";
 	
-	size_t length; // NOTE: size_t = unsigned long int
-	std::cin >>length;
+  size_t length; // NOTE: size_t = unsigned long int
+  std::cin >>length;
 
-	// allocate some memory to hold our array
-	long double userArray[length];
+  // allocate some memory to hold our array
+  long double userArray[length];
 
-	std::cout <<"Enter " <<length <<" numbers:\n";
+  std::cout <<"Enter " <<length <<" numbers:\n";
 
-	for (size_t i = 0; i < length; i++)
-		std::cin >>userArray[i];
+  for (size_t i = 0; i < length; i++)
+    std::cin >>userArray[i];
 	
-	//sort the array
-	sortArray(userArray, length);	
+  //sort the array
+  sortArray(userArray, length);	
 
 
-	// print the array
-	std::cout <<"\nin numerical order:\n";
+  // print the array
+  std::cout <<"\nin numerical order:\n";
  
- 	std::cout <<userArray[0];	
-	for (size_t i = 1; i < length; i++)
-		std::cout <<", " <<userArray[i];
+  std::cout <<userArray[0];	
+  for (size_t i = 1; i < length; i++)
+    std::cout <<", " <<userArray[i];
 
-	// terminating newlines are nice
-	std::cout <<std::endl;
+  // terminating newlines are nice
+  std::cout <<std::endl;
 
 
 }
