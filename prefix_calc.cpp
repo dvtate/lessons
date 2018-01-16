13a0efac0bad5f50c5472f16e40bce49e8d4ccd8
Dustin Van Tate Testa <toast27@gmail.com>
2018-01-16 06:10:24 -0500

    a very c++ polish notation calculator
    - string parsing
    - recursion
    - ifs 
    - loops
    - functions
    - iosteam
    
    
    The expression * 2 + 1 3 is the same as 2 * (1 + 3)

--- prefix_calc.cpp
+++ prefix_calc.cpp
@@ -1,33 +1,42 @@
 #include <iostream> // std::cin std::cout std::endl std::string
 #include <string> // std::string std::stof
 
-// this gets a value
+/* Polish notation:
+* operators precede the numbers they act on.
+* one benefit is there is no need for order of operations.
+* The expression `* 2 + 1 3` is the same as `2 * (1 + 3)`
+*/
+
+// this gets a value/evaluates an operator
 float procExpr() {
 	
 	// this could be a number or an operator
 	std::string valOrOp;
-	std::cin >>valOrOp;
+	std::cin >>valOrOp; // std::cin tokenizes the input for us
 	
+	// addition
 	if (valOrOp == "+") 
-		return procExpr() + procExpr();
+		return procExpr() + procExpr(); // recursion
 		
+	// subtraction
 	else if (valOrOp == "-")
 		return procExpr() - procExpr();
 	
+	// multiplication
 	else if (valOrOp == "*")
 		return procExpr() * procExpr();
 	
+	// division
 	else if (valOrOp == "/")
 		return procExpr() / procExpr();
 	
-	else // it's a number
-		return std::stof(valOrOp);
+	else // it's a number (hopefully)
+		return std::stof(valOrOp); // convert input to a float
 			
 }
 
+// calculator prompt
 int main() {
-		while (true)
-			std::cout <<">" <<procExpr() <<std::endl;
-
+	while (true)
+		std::cout <<">" <<procExpr() <<std::endl;
 }
-
